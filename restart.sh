d96bad67599311a7c5bcb9c9f953141fea69a035
huy <dr.huy69@gmail.com>
2021-03-11 02:18:33 +0700

    init

--- restart.sh
+++ restart.sh
@@ -6,5 +6,5 @@ kill -15 $(pgrep -f "rtsp://192.168.1.38:554/user=admin_password=_channel=0_stre
 
 #RTSP_PATH=rtsp://aivisvn.ddns.net:60960/user=admin_password=aiv032020_channel=1_stream=0.sdp
 RTSP_PATH=rtsp://192.168.1.38:554/user=admin_password=_channel=0_stream=0.sdp
-ffmpeg -v info -rtsp_transport tcp -i $RTSP_PATH -vf scale=1920x1080 -bufsize 1835k -pix_fmt yuv420p -flags -global_header -hls_time 30 -hls_list_size 6 -hls_wrap 10 -start_number 1 static/video/video.m3u8
+ffmpeg -v info -rtsp_transport tcp -i $RTSP_PATH -vf scale=1920x1080 -bufsize 1835k -pix_fmt yuv420p -flags -global_header -hls_time 30 -hls_list_size 6 -hls_wrap 10 -start_number 1 static/video/video.m3u8 &
 venv/bin/python live_camera_server.py > live_camera_server.log 2>&1 &
