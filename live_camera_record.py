bc59bca8183522774eed19b7357aadf24e644075
huy <dr.huy69@gmail.com>
2021-03-15 02:08:44 +0700

    init

--- live_camera_record.py
+++ live_camera_record.py
@@ -3,7 +3,7 @@ import time
 
 def record():
     ffmpegCMD = 'ffmpeg -rtsp_transport tcp -i rtsp://192.168.1.38:554/user=admin_password=_channel=0_stream=0.sdp ' \
-                '-c:v copy -c:a copy -bufsize 1835k -pix_fmt yuv420p -flags -global_header ' \
+                '-flags -global_header ' \
                 '-hls_time 10 -hls_list_size 6 -hls_wrap 10 ' \
                 '-start_number 1 static/video/video.m3u8'
     while True:
